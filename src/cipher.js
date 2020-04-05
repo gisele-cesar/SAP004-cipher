9c00a1fef45a53f114bf838ab80fe1f59bb97c1f
Gisele Cesar <gisele-cesar@hotmail.com>
2020-04-05 20:59:06 -0300

    feito lógica js em cipher.js e index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
 const cipher = {
-  // ...
-};
+  encode (deslocamento, textoNaoProcessado) {
+    textoNaoProcessado = textoNaoProcessado.toUpperCase();
+    let mensagemNaoProcessada = [];
+    let mensagemProcessada = "";
+    
+    deslocamento = Number(deslocamento);
+
+    for (let i=0;i<textoNaoProcessado.length;i++) {
+      mensagemNaoProcessada[i] = (textoNaoProcessado.charAt(i));
+      mensagemNaoProcessada[i] = (((mensagemNaoProcessada[i].charCodeAt(0)-65 + deslocamento) % 26) + 65);
+      
+      if(textoNaoProcessado[i] != " ")
+        mensagemProcessada += String.fromCharCode(mensagemNaoProcessada[i]);
+      else
+        mensagemProcessada += " ";
+    }
+    return mensagemProcessada;
+  },
+  decode (deslocamento, textoNaoProcessado) {
+    textoNaoProcessado = textoNaoProcessado.toUpperCase();
+    let mensagemNaoProcessada = [];
+    let mensagemProcessada = "";
+    
+    deslocamento = Number(deslocamento);
+
+    for (let i=0;i<textoNaoProcessado.length;i++) {
+      mensagemNaoProcessada[i] = (textoNaoProcessado.charAt(i));
+      mensagemNaoProcessada[i] = (((mensagemNaoProcessada[i].charCodeAt(0)-65 - deslocamento) % 26) + 65);
+      
+      if(textoNaoProcessado[i] != " ")
+        mensagemProcessada += String.fromCharCode(mensagemNaoProcessada[i]);
+      else
+        mensagemProcessada += " ";
+    }
+    return mensagemProcessada;
+  }
+   };
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
 
+
+document.getElementById("escolhaCifrar").addEventListener ("click", cifrar)
+document.getElementById("escolhaDecifrar").addEventListener ("click", decifrar)
+
+function cifrar() {
+    let deslocamento = document.getElementById("deslocamento").value;
+    let textoNaoProcessado = document.getElementById("textoNaoProcessado").value;
+    let textoResultado = cipher.encode (deslocamento, textoNaoProcessado);
+    document.getElementById("textoResultado").innerHTML = textoResultado;
+}
+
+function decifrar() {
+    let deslocamento = document.getElementById("deslocamento").value;
+    let textoNaoProcessado = document.getElementById("textoNaoProcessado").value;
+    let textoResultado = cipher.decode (deslocamento, textoNaoProcessado);
+    document.getElementById("textoResultado").innerHTML = textoResultado;
+}
+
+
 console.log(cipher);
