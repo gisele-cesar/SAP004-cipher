066a27bce79557b0f1e7dc3c374af460e7212fd4
Gisele Cesar <gisele-cesar@hotmail.com>
2020-04-06 17:24:16 -0300

    resolvido o problema de decifrar no arquivo cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -26,7 +26,7 @@ const cipher = {
 
     for (let i=0;i<textoNaoProcessado.length;i++) {
       mensagemNaoProcessada[i] = (textoNaoProcessado.charAt(i));
-      mensagemNaoProcessada[i] = (((mensagemNaoProcessada[i].charCodeAt(0)-65 - deslocamento) % 26) + 65);
+      mensagemNaoProcessada[i] = (((mensagemNaoProcessada[i].charCodeAt(0)-90 - deslocamento) % 26) + 90);
       
       if(textoNaoProcessado[i] != " ")
         mensagemProcessada += String.fromCharCode(mensagemNaoProcessada[i]);
