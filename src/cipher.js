a3e9016123ea5f5817a5ca35fa68ed9dc1f6171c
Gisele Cesar <gisele-cesar@hotmail.com>
2020-04-12 19:24:55 -0300

    retirado comentários do meio do código no arquivo cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,50 +1,41 @@
 const cipher = {
-  encode (deslocamento, textoNaoProcessado) {
+  encode(deslocamento, textoNaoProcessado) {
     textoNaoProcessado = textoNaoProcessado.toUpperCase();
     let mensagemNaoProcessada = [];
     let mensagemProcessada = "";
-    
+
     deslocamento = Number(deslocamento);
 
-    for (let i=0;i<textoNaoProcessado.length;i++) {
+    for (let i = 0; i < textoNaoProcessado.length; i++) {
       mensagemNaoProcessada[i] = (textoNaoProcessado.charAt(i));
-      mensagemNaoProcessada[i] = (((mensagemNaoProcessada[i].charCodeAt(0)-65 + deslocamento) % 26) + 65);
-      
-      if(textoNaoProcessado[i] != " ")
+      mensagemNaoProcessada[i] = (((mensagemNaoProcessada[i].charCodeAt(0) - 65 + deslocamento) % 26) + 65);
+
+      if (textoNaoProcessado[i] != " ")
         mensagemProcessada += String.fromCharCode(mensagemNaoProcessada[i]);
       else
         mensagemProcessada += " ";
-        
-       /* if(textoNaoProcessado.charCodeAt[i] <= 90 && textoNaoProcessado.charCodeAt[i] >= 65)
-        {
-          mensagemProcessada += String.fromCharCode(mensagemNaoProcessada[i]);
-        }        
-        else
-        {
-          mensagemProcessada += textoNaoProcessado.charAt[i]
-        }   */     
     }
     return mensagemProcessada;
   },
-  decode (deslocamento, textoNaoProcessado) {
+  decode(deslocamento, textoNaoProcessado) {
     textoNaoProcessado = textoNaoProcessado.toUpperCase();
     let mensagemNaoProcessada = [];
     let mensagemProcessada = "";
-    
+
     deslocamento = Number(deslocamento);
 
-    for (let i=0;i<textoNaoProcessado.length;i++) {
+    for (let i = 0; i < textoNaoProcessado.length; i++) {
       mensagemNaoProcessada[i] = (textoNaoProcessado.charAt(i));
-      mensagemNaoProcessada[i] = (((mensagemNaoProcessada[i].charCodeAt(0)-90 - deslocamento) % 26) + 90);
-      
-      if(textoNaoProcessado[i] != " ")
+      mensagemNaoProcessada[i] = (((mensagemNaoProcessada[i].charCodeAt(0) - 90 - deslocamento) % 26) + 90);
+
+      if (textoNaoProcessado[i] != " ")
         mensagemProcessada += String.fromCharCode(mensagemNaoProcessada[i]);
       else
         mensagemProcessada += " ";
     }
     return mensagemProcessada;
   }
-   };
+};
 
 
 export default cipher;
